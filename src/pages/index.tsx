f4480a8430e9e9fc334d2c9d98e7c5ac0a3ff77f
Hans Juno <123650964+hansjunodev@users.noreply.github.com>
2023-01-31 14:43:54 -0500

    feat: add timer functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [timeInvested, setTimeInvested] = useState(0);
+  const [startTime, setStartTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  function handleStartClick() {
+    setStartTime(Date.now());
+    setIsRunning(true);
+  }
+
+  function handleStopClick() {
+    setIsRunning(false);
+  }
+
+  useEffect(() => {
+    if (isRunning) {
+      const timer = setTimeout(() => {
+        setTimeInvested(timeInvested + Date.now() - startTime);
+        // setCookie('timeInvested', timeInvested)
+        setStartTime(Date.now());
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [startTime, timeInvested, isRunning]);
+
   return (
     <>
       <Head>
@@ -14,10 +40,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <input type="text" defaultValue="Learning Piano"/>
-        <div>1h : 39m : 12s</div>
-        <button>Start</button>
-        <button>Stop</button>
+        <input type="text" defaultValue="Learning Piano" />
+        <div>{Math.floor(timeInvested / 1000)} s</div>
+        <button onClick={handleStartClick}>Start</button>
+        <button onClick={handleStopClick}>Stop</button>
       </main>
     </>
   );
