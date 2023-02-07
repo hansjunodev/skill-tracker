1e1a50f1498163b417fda1c2b1aff9e58889b0e0
Hans Juno <123650964+hansjunodev@users.noreply.github.com>
2023-02-07 11:57:05 -0500

    fix: display the correct seconds elapsed

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -6,4 +6,19 @@ test("convert milliseconds into a readable time duration", () => {
     minutes: 0,
     seconds: 0,
   });
+  expect(toTimeObject(3637000)).toStrictEqual({
+    hours: 1,
+    minutes: 0,
+    seconds: 37,
+  });
+  expect(toTimeObject(4417000)).toStrictEqual({
+    hours: 1,
+    minutes: 13,
+    seconds: 37,
+  });
+  expect(toTimeObject(3.312e+8)).toStrictEqual({
+    hours: 92,
+    minutes: 0,
+    seconds: 0,
+  });
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,7 +8,7 @@ export function toTimeObject(milliseconds: number): Time {
   const hours = parseInt(milliseconds / (1000 * 60 * 60));
   milliseconds = milliseconds % 3600000;
   const minutes = parseInt(milliseconds / (1000 * 60));
-  milliseconds = milliseconds % 6000;
+  milliseconds = milliseconds % 60000;
   const seconds = parseInt(milliseconds / 1000);
 
   return {
