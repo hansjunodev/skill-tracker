ffa42c3d9d8971afff1f067129a3fc3ec4c26634
Hans Juno <123650964+hansjunodev@users.noreply.github.com>
2023-02-06 20:14:16 -0500

    refactor: change let to const when possible

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,15 +5,11 @@ interface Time {
 }
 
 export function toTimeObject(milliseconds: number): Time {
-  let hours, minutes, seconds;
-
-  hours = parseInt(milliseconds / (1000 * 60 * 60));
+  const hours = parseInt(milliseconds / (1000 * 60 * 60));
   milliseconds = milliseconds % 3600000;
-
-  minutes = parseInt(milliseconds / (1000 * 60));
+  const minutes = parseInt(milliseconds / (1000 * 60));
   milliseconds = milliseconds % 6000;
-
-  seconds = parseInt(milliseconds / 1000);
+  const seconds = parseInt(milliseconds / 1000);
 
   return {
     hours,
