7d063ca4e2cd739626809b1ea0e25fb5c8061ae6
Hans Juno <123650964+hansjunodev@users.noreply.github.com>
2023-02-08 21:23:34 -0500

    test: add a util test

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,6 +1,11 @@
 import { toTimeObject } from "@/utils/utils";
 
 test("convert milliseconds into a readable time duration", () => {
+  expect(toTimeObject(0)).toStrictEqual({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
   expect(toTimeObject(3600000)).toStrictEqual({
     hours: 1,
     minutes: 0,
