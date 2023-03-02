34239aa3e68d91ee9bc53ad36734fadca1910c4d
Hans Juno <123650964+hansjunodev@users.noreply.github.com>
2023-03-02 13:44:23 -0500

    fix: prevent kbd from filling container

--- src/components/Kbd.tsx
+++ src/components/Kbd.tsx
@@ -4,7 +4,7 @@ interface KbdProps {
 
 export default function Kbd({ children }: KbdProps): JSX.Element {
   return (
-    <kbd className="cursor-default select-none whitespace-nowrap rounded border border-solid border-black bg-gray-100 px-2 py-1 text-sm font-medium leading-none text-black shadow-kbd">
+    <kbd className="m-auto cursor-default select-none whitespace-nowrap rounded border border-solid border-black bg-gray-100 px-2 py-1 text-sm font-medium leading-none text-black shadow-kbd">
       {children}
     </kbd>
   );
