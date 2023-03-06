afcac7240bf3db56c37a46c97f864170502000ef
Hans Juno <123650964+hansjunodev@users.noreply.github.com>
2023-03-05 23:00:19 -0500

    refactor: extract card title as a component

---
+++ src/components/EditableTitle.tsx
@@ -0,0 +1,56 @@
+import useOutsideClick from "@/hooks/useOutsideClick";
+import { useCallback, useEffect, useState } from "react";
+
+export default function EditableTitle(): JSX.Element {
+  const [titleText, setTitleText] = useState("Untitled");
+  const [isEditing, setIsEditing] = useState(false);
+  const handleOutsideClick = useCallback(() => {
+    if (isEditing) setIsEditing(false);
+  }, [isEditing, titleText]);
+
+  const ref = useOutsideClick(handleOutsideClick);
+
+  const handleTextChange = (e: InputEvent) => {
+    setTitleText(e.target.value);
+    e.stopPropagation();
+  };
+
+  const handleInputClick = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  const handleLabelClick = (e: MouseEvent) => {
+    setIsEditing(!isEditing);
+    e.stopPropagation();
+  };
+
+  useEffect(() => ref.current?.focus(), [ref, isEditing]);
+
+  let content;
+
+  if (isEditing) {
+    content = (
+      <input
+        ref={ref}
+        className="bg-none"
+        type="text"
+        onChange={handleTextChange}
+        onClick={handleInputClick}
+        value={titleText}
+      />
+    );
+  } else {
+    // content = skill.title;
+    content = (
+      <button
+        className="cursor-text rounded px-1 text-black hover:shadow hover:shadow-black"
+        onClick={handleLabelClick}
+        type="button"
+      >
+        {titleText}
+      </button>
+    );
+  }
+
+  return content;
+}
